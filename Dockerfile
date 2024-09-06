091f65db1ba292c0bc65c7cdcf940d52366168ff
Prudhvi Gopasina <110560887+pgopasina@users.noreply.github.com>
2024-09-06 23:32:10 +0530

    Initial commit

---
+++ Dockerfile
@@ -0,0 +1,22 @@
+# Use an official Python runtime as a base image
+FROM python:3.9-slim
+
+# Set environment variables
+ENV PYTHONDONTWRITEBYTECODE 1
+ENV PYTHONUNBUFFERED 1
+
+# Set the working directory
+WORKDIR /app
+
+# Copy the current directory contents into the container at /app
+COPY . /app
+
+# Install any needed dependencies
+RUN pip install --upgrade pip
+RUN pip install -r requirements.txt
+
+# Expose the port the app runs on
+EXPOSE 5000
+
+# Run the app
+CMD ["python", "app.py"]
---
+++ app.py
@@ -0,0 +1,89 @@
+from flask import Flask, jsonify, request
+from flask_sqlalchemy import SQLAlchemy
+import json
+app = Flask(__name__)
+
+with open('config.json') as f:
+        config_json = json.load(f)
+        
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://{}:{}@{}/{}'.format(config_json['username'], config_json['password'], config_json['hostname'], config_json['database'])
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"]=False 
+
+db = SQLAlchemy(app)
+
+class Users(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
+    role = db.Column(db.String(50), nullable=False)
+    email = db.Column(db.String(50), unique=True, nullable=False)
+
+# deprecated
+# @app.before_first_request
+# def createTable():
+#     db.create_all()
+    
+# @app.before_request
+# def createTable():
+#     app.before_request_funcs[none].remove(create_tables)
+#     db.create_all()
+
+with app.app_context():
+    db.create_all()
+
+@app.route('/users',methods=['POST'])
+def createUser():
+    data = request.get_json()
+    new_user = Users(name = data['name'], role = data['role'], email = data['email'])
+    db.session.add(new_user)
+    db.session.commit()
+    
+    userData = {
+        'id': new_user.id,
+        'name': new_user.name,
+        'role': new_user.role,
+        'email': new_user.email
+    }
+    return jsonify({'message': 'User created successfully','user': userData}), 200
+
+@app.route('/users', methods=['GET'])
+def getUsers():
+    users = Users.query.all()
+    # Get users get based on ID
+    # user = Users.query.get_or_404(id)
+    return jsonify([{'id':user.id,'name':user.name,'role':user.role,'email':user.email} for user in users]), 200
+    
+@app.route("/users/<int:id>", methods=['PUT'])
+def updateUser(id):
+    user = Users.query.get_or_404(id)
+    data = request.get_json()
+
+    user.name = data.get('name', user.name)
+    user.role = data['role']
+    user.email = data['email']
+
+    db.session.commit()
+
+    updatedUser = {
+        'id': user.id,
+        'name': user.name,
+        'role': user.role,
+        'email': user.email
+    }
+
+    return jsonify({"message": "User updated successfully", "updated_user": updatedUser}), 200
+    
+
+@app.route('/users/<int:id>', methods=['DELETE'])
+def deleteUser(id):
+    user = Users.query.get_or_404(id)
+    db.session.delete(user)
+    db.session.commit()
+    return jsonify({'message': 'User deleted successfully'}),200
+
+
+
+if __name__ == "__main__":
+    app.run()
+
+# if __name__ == "__main__":
+#     app.run(host="0.0.0.0", port=8080, debug=True)
